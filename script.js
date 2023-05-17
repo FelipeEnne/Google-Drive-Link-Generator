ce872b41e025afd2ce5ea649d009b4aa8a44e760
FelipeEnne <felipeenne@gmail.com>
2023-05-16 21:09:41 -0300

    finish

--- script.js
+++ script.js
@@ -36,5 +36,32 @@ function generateLink(e) {
     copy.addEventListener("click", () => {
       return copyText(downloadLink);
     });
+
+    // EMBED AUDIO
+    const audio1 = '<audio width="300" height="32" controls="controls" src="';
+    const audio2 = '" type="audio/mp3"></audio>';
+    const embedAudio = document.getElementById("embed-audio");
+    embedAudio.value = `${audio1}${downloadLink.value}${audio2}`;
+
+    const copyAudio = document.querySelector(".copy-audio");
+    copyAudio.addEventListener("click", () => {
+      return copyText(embedAudio);
+    });
+
+    // EMBED VIDEO
+    const getVideoLink = gLink.value.replace("/view?usp=sharing", "");
+
+    const video1 = '<iframe src="';
+    const video2 = '/preview" width="560" height="315"></iframe>';
+
+    const embedVideo = document.getElementById("embed-video");
+    embedVideo.value = `${video1}${getVideoLink}${video2}`;
+
+    const copyVideo = document.querySelector(".copy-video");
+    copyVideo.addEventListener("click", () => {
+      return copyText(embedVideo);
+    });
+  } else {
+    alert("Please Type Google Drive Link");
   }
 }
